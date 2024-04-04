d004f78232d99f91c7cf86c8db8edf4a33acd746
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-04-04 10:35:09 +0700

    Listing 8.1

---
+++ Pertemuan 7/PairtTest1.java
@@ -0,0 +1,8 @@
+public class PairtTest1 {
+    public static void main(String[] args) {
+        String[] words = {"Mary", "had", "a", "little", "lamb"};
+        Pair<String> mm = ArrayAlg.minmax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+    }
+}
